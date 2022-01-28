d3628ed4c6de1806feb37ef559b4e45dd99eb04f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2022-01-28 15:56:29 +0700

    Add tests

--- lib/clipboard.ex
+++ lib/clipboard.ex
@@ -7,12 +7,24 @@ defmodule Clipboard do
 
   @doc """
   Copy `content` into clipboard.
+
+  ## Examples
+
+      iex> Clipboard.copy("Hello")
+      :ok
   """
   @spec copy(String.t()) :: :ok | :error
   def copy(content) when is_binary(content), do: NIF.copy(content)
 
   @doc """
   Get the content from clipboard.
+
+  ## Examples
+
+      iex> Clipboard.copy("Hello")
+      :ok
+      iex> Clipboard.paste()
+      {:ok, "Hello"}
   """
   @spec paste() :: {:ok, String.t()} | :error
   def paste(), do: NIF.paste()
